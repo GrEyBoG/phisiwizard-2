962f42d509d6276c8d82724558cbbf5322f784cc
LUISMG30 <luismiguelgranados6@gmail.com>
2023-05-17 21:13:14 -0500

    este si es att:velez

--- scripts.js
+++ scripts.js
@@ -2,22 +2,53 @@
 
 function calcularMCU() {
     // Obtenemos los valores ingresados por el usuario
-    const radio = document.getElementById("radio").value;
-    const tiempo = document.getElementById("tiempo").value;
+    var radio = document.getElementById("radio").value;
+    var tiempo = document.getElementById("tiempo").value;
 
     // Calculamos la velocidad angular del objeto
-    const velocidadAngular = 2 * Math.PI / tiempo;
+    var velocidadAngular = 2 * Math.PI / tiempo;
 
     // Calculamos la aceleración centrípeta del objeto
-    const aceleracionCentripeta = velocidadAngular ** 2 * radio;
+    var aceleracionCentripeta = velocidadAngular ** 2 * radio;
 
     // Calculamos la velocidad tangencial del objeto
-    const velocidadTangencial = velocidadAngular * radio;
+    var velocidadTangencial = velocidadAngular * radio;
 
     // Mostramos los resultados al usuario
     document.getElementById("resultado").innerHTML = `La velocidad angular del objeto es: ${velocidadAngular.toFixed(2)} rad/s.<br>
 La aceleración centrípeta del objeto es: ${aceleracionCentripeta.toFixed(2)} m/s².<br>
 La velocidad tangencial del objeto es: ${velocidadTangencial.toFixed(2)} m/s.`}
+
+function mostrarProcedimientoMCU() {
+    // Obtenemos los valores ingresados por el usuario
+    var radio = parseFloat(document.getElementById("radio").value);
+    var tiempo = parseFloat(document.getElementById("tiempo").value);
+  
+    // Calculamos la velocidad angular del objeto
+    var velocidadAngular = 2 * Math.PI / tiempo;
+  
+    // Mostramos el procedimiento y el resultado al usuario
+    document.getElementById("procedimientoMCU").innerHTML = `Calculamos la velocidad angular con la fórmula: ω = 2π / T` +
+      `<br>ω = 2π / ${tiempo}` +
+      `<br>La velocidad angular del objeto es: ${velocidadAngular.toFixed(2)} rad/s.`;
+  
+    // Calculamos la aceleración centrípeta del objeto
+    var aceleracionCentripeta = velocidadAngular ** 2 * radio;
+  
+    // Mostramos el procedimiento y el resultado al usuario
+    document.getElementById("procedimientoMCU").innerHTML += `<br>Calculamos la aceleración centrípeta con la fórmula: ac = ω²r` +
+      `<br>ac = (${velocidadAngular.toFixed(2)})² * ${radio}` +
+      `<br>La aceleración centrípeta del objeto es: ${aceleracionCentripeta.toFixed(2)} m/s².`;
+  
+    // Calculamos la velocidad tangencial del objeto
+    const velocidadTangencial = velocidadAngular * radio;
+  
+    // Mostramos el procedimiento y el resultado al usuario
+    document.getElementById("procedimientoMCU").innerHTML += `<br>Calculamos la velocidad tangencial con la fórmula: vt = ωr` +
+      `<br>vt = ${velocidadAngular.toFixed(2)} * ${radio}` +
+      `<br>La velocidad tangencial del objeto es: ${velocidadTangencial.toFixed(2)} m/s.`;
+  }
+  
 
 function calcularVelocidad() {
     // Pedir valores de distancia y tiempo
@@ -31,6 +62,21 @@ function calcularVelocidad() {
     document.getElementById("resultado2").innerHTML = `La velocidad es de ${velocidad} m/s.`;
   }
 
+  function mostrarProcedimientoMRU() {
+    // Obtener valores ingresados por el usuario
+    const distancia = parseFloat(document.getElementById("distancia").value);
+    const tiempo = parseFloat(document.getElementById("tiempo2").value);
+
+    // Calcular la velocidad
+    const velocidad = distancia / tiempo;
+
+    // Mostrar el procedimiento y el resultado al usuario
+    document.getElementById("procedimientoMRU").innerHTML = `Calculamos la velocidad con la fórmula: v = d / t` +
+        `<br>v = ${distancia} / ${tiempo}` +
+        `<br>La velocidad es de ${velocidad} m/s.`;
+}
+
+
 function calcularEcuacionHoraria() {
     // Obtenemos los valores ingresados por el usuario
     const x0 = document.getElementById("x0").value;
@@ -43,6 +89,22 @@ function calcularEcuacionHoraria() {
     // Mostramos el resultado al usuario
     document.getElementById("resultado3").innerHTML = `La posición del objeto es: x = ${x} m.`;
 }
+
+function mostrarProcedimientoEcuacionHoraria() {
+    // Obtenemos los valores ingresados por el usuario
+    const x0 = parseFloat(document.getElementById("x0").value);
+    const v = parseFloat(document.getElementById("v").value);
+    const t = parseFloat(document.getElementById("t").value);
+  
+    // Calculamos la posición del objeto en función del tiempo
+    const x = x0 + v * t;
+  
+    // Mostramos el procedimiento y el resultado al usuario
+    document.getElementById("procedimientoEcuacionHoraria").innerHTML = `Calculamos la posición del objeto en función del tiempo con la fórmula: x = x0 + v * t` +
+      `<br>x = ${x0} + ${v} * ${t}` +
+      `<br>La posición del objeto es: x = ${x} m.`;
+  }
+  
 
 // function calcularEcuacion() {
 //     // Valores de las coordenadas ingresadas por el usuario
